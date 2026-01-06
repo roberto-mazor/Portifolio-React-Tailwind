48ed6d7e6c5b19c86e5cc821d35526936bed5042
roberto-mazor <mazor.estudo@gmail.com>
2026-01-05 21:49:52 -0300

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,7 @@ import { Github, ExternalLink, ChevronDown, ChevronUp } from 'lucide-react';
 
 const projectList = [
     {
-        title: "Dashboard Financeiro Inteligente",
+        title: "Dashboard Financeiro Inteligente (Full-Stack)",
         image: "/public/previa_dashboard_financeiro.png", // Adicione suas imagens na pasta public
         description: "Gestão financeira completa com visualização de dados em tempo real e suporte a temas dinâmicos (Light/Dark). O projeto implementa uma arquitetura robusta com autenticação JWT e uma estratégia exclusiva de 'Wake-up Call' para otimizar o tempo de resposta do banco de dados PostgreSQL serverless.",
         tags: ["React", "Node.js", "PostgreSQL", "MUI X Charts", "Tailwind CSS"],
